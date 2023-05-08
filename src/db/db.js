f522af82a746fee9e459b09f79c33063d77132de
Isabel Abad <isabelabad82@gmail.com>
2023-05-08 12:28:51 +0200

    Estructura básica API

---
+++ src/db/db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2/promise');
+
+require('dotenv').config();
+
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+
+let pool;
+
+const getDB = async () => {
+  if (!pool) {
+    pool = mysql.createPool({
+      connectionLimit: 10,
+      host: MYSQL_HOST,
+      user: MYSQL_USER,
+      password: MYSQL_PASSWORD,
+      database: MYSQL_DATABASE,
+      timezone: 'Z',
+    });
+  }
+  return await pool.getDB();
+};
+
+module.exports = getDB;
