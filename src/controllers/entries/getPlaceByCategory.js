d9c2758d770ddfd1c408cfd0f76427125cceffde
IsabelAbad <isabelabad82@gmail.com>
2023-07-05 19:19:00 +0200

    añado fotos a categorielist

--- src/controllers/entries/getPlaceByCategory.js
+++ src/controllers/entries/getPlaceByCategory.js
@@ -6,11 +6,12 @@ const getPlacesByCategory = async (req, res) => {
   try {
     connect = await getDB();
     const [result] = await connect.query(
-      `SELECT c.id as category_id, c.name as category_name, sum(v.vote)/count(v.vote) as votes_average, p.*
+      `SELECT c.id as category_id, c.name as category_name, sum(v.vote)/count(v.vote) as votes_average, p.*, photos.photo
       FROM categories c
       INNER JOIN place_category pc ON c.id = pc.category_id
       INNER JOIN places p ON p.id = pc.place_id
       LEFT JOIN votes v ON p.id = v.place_id
+      LEFT JOIN photos ON p.id = photos.place_id
       WHERE c.id = ?`,
       [id]
     );
@@ -18,8 +19,8 @@ const getPlacesByCategory = async (req, res) => {
     if (result.length === 0) {
       return res.status(404).send({
         status: 'error',
-        message: "The category doesn't exist"
-      })
+        message: "The category doesn't exist",
+      });
     }
 
     res.status(200).send({
@@ -30,8 +31,8 @@ const getPlacesByCategory = async (req, res) => {
   } catch (err) {
     res.status(500).send({
       status: 'error',
-      message: err.message
-    })
+      message: err.message,
+    });
   } finally {
     if (connect) connect.release();
   }
